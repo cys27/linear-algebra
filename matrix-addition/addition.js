70bf76ba1d8f9c8ba482801fc9fc9abfe67e90c4
enes <139135923+cys27@users.noreply.github.com>
2024-03-10 19:54:15 +0300

    Matrix Addition

---
+++ matrix-addition/addition.js
@@ -0,0 +1,45 @@
+const addMatrix = equation => {
+    equation = equation.replace(/ /g, "");
+    const matrices = equation.split("+").map(matrix => JSON.parse(matrix));;
+
+    const matrixLength = [], vectorLength = [], typeofVectorItems = [];
+    for (const matrix of matrices) {
+        matrixLength.push(matrix.length);
+        if ((new Set(matrixLength)).size !== 1) return false;
+
+        for (const vector of matrix) {
+            vectorLength.push(vector.length);
+            if ((new Set(vectorLength)).size !== 1) return false;
+
+            vector.map(n => {
+                typeofVectorItems.push(typeof n);
+            });
+        }
+    }
+
+    for (const types of typeofVectorItems) {
+        if (types !== "number") return false;
+    }
+
+    const result = [];
+    for (let i = 0; i < matrixLength[0]; i++) {
+        result.push([]);
+    }
+
+    for (const matrix of matrices) {
+        for (let i = 0; i < matrix.length; i++) {
+            result[i].push(matrix[i])
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        result[i] = result[i].reduce((a, b) => a.map((c, d) => b[d] + c), new Array(result[i][0].length).fill(0));
+    }
+    return result;
+}
+
+/*
+    addMatrix(
+        "[[-2,-4,-6], [-6,-8,-10], [-12,-14,-16]] + [[1,2,3], [3,4,5], [6,7,8]] + [[1,2,3], [3,4,5], [6,7,8]]"
+    )
+*/
